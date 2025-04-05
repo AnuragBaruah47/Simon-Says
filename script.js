8f672826720cc07caba102bca23dccbfe614b969
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-04-05 20:54:57 +0530

    Wrting main game logic else almost done

--- script.js
+++ script.js
@@ -9,38 +9,66 @@ let userInput=[]
 let allTheButtonsId=["red","green","yellow","blue"]
 // all the fucntions
 const startGame=()=>{
-    return game=true
+    addEventListener("keypress",()=>{
+        return game=true
+    })
+    round=1
+    roundUpdater.innerText=`Round ${round}`
 }
 
 const mainGame=()=>{
-    addEventListener("keypress",()=>{
-        startGame()
-        if (game===true) {
-            colorBlink()
-            console.log(pattersArray)
-            allButton.forEach((button)=>{
-
-                button.addEventListener("click",()=>{
-
-                })
-            })
-        }
-    })
+    
 }
 const colorBlink=()=>{
-    randomNumber=(Math.random() * 3 + 1).toFixed(0)
-    pattersArray.push(randomNumber)
-    const blinkButton=document.getElementById(allTheButtonsId[(randomNumber)-1])
-    setTimeout(()=>{
-        blinkButton.style.backgroundColor=`${allTheButtonsId[(randomNumber)-1]}`
-        console.log(allTheButtonsId[(randomNumber)-1]);
-    },350)
-    blinkButton.style.backgroundColor="  #FFFFFF"
+    if (game===true){
+        randomNumber=(Math.random() * 3 + 1).toFixed(0)
+        pattersArray.push(allTheButtonsId[(randomNumber)-1])
+        const blinkButton=document.getElementById(allTheButtonsId[(randomNumber)-1])
+        setTimeout(()=>{
+            blinkButton.style.backgroundColor=`${allTheButtonsId[(randomNumber)-1]}`
+        },350)
+        blinkButton.style.backgroundColor="  #FFFFFF"
+    }
 }
 
 const endGame=()=>{
     roundUpdater.innerText="Game Over!"
+    scoreBox.innerText=`Your Score = ${round}`
     return game=false
 }
 
 mainGame()
+
+
+
+
+
+
+
+
+
+// const mainGame=()=>{
+//     addEventListener("keypress",()=>{
+//         startGame()
+//         if (game===true) {
+//             colorBlink()
+//             console.log(pattersArray)
+//             allButton.forEach((button)=>{
+//                 button.addEventListener("click",()=>{
+//                     userInput.push(button.id)
+//                     const userInputString=userInput.join(" ")
+//                     console.log(userInputString);
+//                     const pattersArrayString=userInput.join(" ")
+//                     console.log(pattersArrayString);
+//                     if (userInputString===pattersArrayString) {
+//                         colorBlink()
+//                         round++
+//                         roundUpdater.innerText=`Round ${round}`
+//                     }else{
+//                         endGame()
+//                     }
+//                 })
+//             })
+//         }
+//     })
+// }
